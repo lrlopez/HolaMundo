128e065f5d819c2b65192d6ff1b58701b3e719d4
Luis Ramón López <lrlopez@gmail.com>
2022-11-30 10:14:01 +0100

    Añadir intensidad al mensaje

--- src/HolaMundoJava.java
+++ src/HolaMundoJava.java
@@ -1,5 +1,5 @@
 public class HolaMundoJava {
     public static void main(String[] args) {
-        System.out.println("Hola mundo Oretano.");
+        System.out.println("¡Hola mundo Oretano!");
     }
 }
